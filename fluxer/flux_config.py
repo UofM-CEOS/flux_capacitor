e1cf91b3bd5a3772d74813cc0ef20da872922dfc
Sebastian Luque <spluque@gmail.com>
2015-08-02 19:54:38 -0500

    Standardized single/double quotes.

--- fluxer/flux_config.py
+++ fluxer/flux_config.py
@@ -9,7 +9,7 @@ import glob
 import re
 
 
-__all__ = ['parse_config']
+__all__ = ["parse_config"]
 
 # We need to control names.  We leave the date time columns (always the
 # first two in input) up to pandas to determine, as they're always safe.
@@ -73,77 +73,77 @@ _incols_all = dict(longitude=float, latitude=float, speed_over_ground=float,
 
 # Simple dictionary to list our defaults
 _dflts = {
-    "ec_input_directory": getcwd(),
-    "ec_file_pattern": '*[0-9].csv',
-    "ec_colnames": ('time_20min,time_study,longitude,latitude,' +
-                    'speed_over_ground,course_over_ground,heading,' +
-                    'pitch,roll,heave,atmospheric_pressure,' +
-                    'air_temperature,relative_humidity,' +
-                    'surface_temperature,wind_speed,wind_direction,' +
-                    'true_wind_speed,true_wind_direction,PAR,K_down,' +
-                    'temperature_thermopile,temperature_case,' +
-                    'temperature_dome,LW_down,UV_sensor_temperature,' +
-                    'UV_b,UV_broad,acceleration_x,acceleration_y,' +
-                    'acceleration_z,rate_x,rate_y,rate_z,wind_speed_u,' +
-                    'wind_speed_v,wind_speed_w,air_temperature_sonic,' +
-                    'sound_speed,anemometer_status,op_CO2_fraction,' +
-                    'op_CO2_density,op_CO2_absorptance,op_H2O_fraction,' +
-                    'op_H2O_density,op_H2O_absorptance,op_pressure,' +
-                    'op_temperature,op_temperature_base,' +
-                    'op_temperature_spar,op_temperature_bulb,' +
-                    'op_cooler_voltage,op_bandwidth,op_delay_interval,' +
-                    'op_bad_chopper_wheel_temperature_flag,' +
-                    'op_bad_detector_temperature_flag,' +
-                    'op_bad_optical_wheel_rate_flag,op_bad_sync_flag,' +
-                    'op_CO2_signal_strength,cp_analyzer_status,' +
-                    'cp_CO2_fraction,cp_CO2_density,cp_CO2_dry_fraction,' +
-                    'cp_CO2_absorptance,cp_H2O_fraction,cp_H2O_density,' +
-                    'cp_H2O_dry_fraction,cp_H2O_absorptance,cp_pressure,' +
-                    'cp_temperature,cp_temperature_in,cp_temperature_out,' +
-                    'cp_temperature_block,cp_temperature_cell,' +
-                    'cp_CO2_signal_strength,cp_H2O_signal_strength'),
-    "ec_sample_frequency": '10.0',
-    "ec_summary_file": 'fluxes.csv',
-    "ec_despike_win_width": '3000',
-    "ec_despike_step": '1500',
-    "ec_despike_nreps": '20',
-    "motion2anemometer_pos": '0.0, 0.0, 0.0',
-    "ec_complementary_filter_period": '10.0',
-    "ec_accel_highpass_cutoff": '20.0',
-    "uw_input_directory": getcwd(),
-    "uw_file_pattern": '*[0-9].csv',
-    "uw_summary_file": 'underway_pCO2.csv',
-    "uw_colnames": ('time_30min,time_study,longitude,latitude,' +
-                    'speed_over_ground,course_over_ground,' +
-                    'atmospheric_pressure,air_temperature,' +
-                    'relative_humidity,surface_temperature,wind_speed,' +
-                    'wind_direction,true_wind_speed,true_wind_direction,' +
-                    'PAR,K_down,LW_down,UV_b,UV_a,UV_broad,' +
-                    'air_temperature_sonic,cp_CO2_fraction,' +
-                    'cp_H2O_fraction,cp_pressure,cp_temperature,' +
-                    'op_CO2_density,op_H2O_density,op_pressure,' +
-                    'op_temperature,equ_temperature,equ_pressure,' +
-                    'equ_speed_pump,uw_CO2_fraction,uw_H2O_fraction,' +
-                    'uw_temperature_analyzer,uw_pressure_analyzer,' +
-                    'H2O_flow,air_flow_analyzer,ctd_pressure,' +
-                    'ctd_temperature,ctd_conductivity,ctd_O2_saturation,' +
-                    'ctd_O2_concentration,uw_pH,uw_redox_potential,' +
-                    'temperature_external,temperature_in,' +
-                    'bad_wind_direction_flag,' +
-                    'very_bad_wind_direction_flag,bad_ice_flag,' +
-                    'bad_atmospheric_pressure_flag,bad_ctd_flag,' +
-                    'bad_CO2_flag,bad_H2O_flag,bad_H2O_flow_flag,' +
-                    'bad_pressure_analyzer_flag,' +
-                    'bad_temperature_analyzer_flag,' +
-                    'bad_equ_temperature_flag,' +
-                    'bad_temperature_external_flag'),
-    "anemometer2D_height": 16}
+    'ec_input_directory': getcwd(),
+    'ec_file_pattern': "*[0-9].csv",
+    'ec_colnames': ("time_20min,time_study,longitude,latitude," +
+                    "speed_over_ground,course_over_ground,heading," +
+                    "pitch,roll,heave,atmospheric_pressure," +
+                    "air_temperature,relative_humidity," +
+                    "surface_temperature,wind_speed,wind_direction," +
+                    "true_wind_speed,true_wind_direction,PAR,K_down," +
+                    "temperature_thermopile,temperature_case," +
+                    "temperature_dome,LW_down,UV_sensor_temperature," +
+                    "UV_b,UV_broad,acceleration_x,acceleration_y," +
+                    "acceleration_z,rate_x,rate_y,rate_z,wind_speed_u," +
+                    "wind_speed_v,wind_speed_w,air_temperature_sonic," +
+                    "sound_speed,anemometer_status,op_CO2_fraction," +
+                    "op_CO2_density,op_CO2_absorptance,op_H2O_fraction," +
+                    "op_H2O_density,op_H2O_absorptance,op_pressure," +
+                    "op_temperature,op_temperature_base," +
+                    "op_temperature_spar,op_temperature_bulb," +
+                    "op_cooler_voltage,op_bandwidth,op_delay_interval," +
+                    "op_bad_chopper_wheel_temperature_flag," +
+                    "op_bad_detector_temperature_flag," +
+                    "op_bad_optical_wheel_rate_flag,op_bad_sync_flag," +
+                    "op_CO2_signal_strength,cp_analyzer_status," +
+                    "cp_CO2_fraction,cp_CO2_density,cp_CO2_dry_fraction," +
+                    "cp_CO2_absorptance,cp_H2O_fraction,cp_H2O_density," +
+                    "cp_H2O_dry_fraction,cp_H2O_absorptance,cp_pressure," +
+                    "cp_temperature,cp_temperature_in,cp_temperature_out," +
+                    "cp_temperature_block,cp_temperature_cell," +
+                    "cp_CO2_signal_strength,cp_H2O_signal_strength"),
+    'ec_sample_frequency': "10.0",
+    'ec_summary_file': "fluxes.csv",
+    'ec_despike_win_width': "3000",
+    'ec_despike_step': "1500",
+    'ec_despike_nreps': "20",
+    'motion2anemometer_pos': "0.0, 0.0, 0.0",
+    'ec_complementary_filter_period': "10.0",
+    'ec_accel_highpass_cutoff': "20.0",
+    'uw_input_directory': getcwd(),
+    'uw_file_pattern': "*[0-9].csv",
+    'uw_summary_file': "underway_pCO2.csv",
+    'uw_colnames': ("time_30min,time_study,longitude,latitude," +
+                    "speed_over_ground,course_over_ground," +
+                    "atmospheric_pressure,air_temperature," +
+                    "relative_humidity,surface_temperature,wind_speed," +
+                    "wind_direction,true_wind_speed,true_wind_direction," +
+                    "PAR,K_down,LW_down,UV_b,UV_a,UV_broad," +
+                    "air_temperature_sonic,cp_CO2_fraction," +
+                    "cp_H2O_fraction,cp_pressure,cp_temperature," +
+                    "op_CO2_density,op_H2O_density,op_pressure," +
+                    "op_temperature,equ_temperature,equ_pressure," +
+                    "equ_speed_pump,uw_CO2_fraction,uw_H2O_fraction," +
+                    "uw_temperature_analyzer,uw_pressure_analyzer," +
+                    "H2O_flow,air_flow_analyzer,ctd_pressure," +
+                    "ctd_temperature,ctd_conductivity,ctd_O2_saturation," +
+                    "ctd_O2_concentration,uw_pH,uw_redox_potential," +
+                    "temperature_external,temperature_in," +
+                    "bad_wind_direction_flag," +
+                    "very_bad_wind_direction_flag,bad_ice_flag," +
+                    "bad_atmospheric_pressure_flag,bad_ctd_flag," +
+                    "bad_CO2_flag,bad_H2O_flag,bad_H2O_flow_flag," +
+                    "bad_pressure_analyzer_flag," +
+                    "bad_temperature_analyzer_flag," +
+                    "bad_equ_temperature_flag," +
+                    "bad_temperature_external_flag"),
+    'anemometer2D_height': 16}
 # Scalar option names
-_scalar_opts = ['ec_sample_frequency', 'ec_despike_win_width',
-                'ec_despike_step', 'ec_despike_nreps',
-                'ec_complementary_filter_period', 'ec_accel_highpass_cutoff',
-                'anemometer2D_height']
-_vector_opts = ['motion2anemometer_pos']
+_scalar_opts = ["ec_sample_frequency", "ec_despike_win_width",
+                "ec_despike_step", "ec_despike_nreps",
+                "ec_complementary_filter_period", "ec_accel_highpass_cutoff",
+                "anemometer2D_height"]
+_vector_opts = ["motion2anemometer_pos"]
 
 def parse_config(cfg_file):
     """Parse configuration file for essential variables during flux analysis.
@@ -162,46 +162,46 @@ def parse_config(cfg_file):
     """
     # Ordered dictionary based on dflts to give to the parser
     dflt_dict = OrderedDict((
-        ('EC Inputs',
+        ("EC Inputs",
          OrderedDict((
-             ('input_directory', _dflts['ec_input_directory']),
-             ('file_pattern', _dflts['ec_file_pattern']),
-             ('colnames', _dflts['ec_colnames']),
-             ('sample_frequency', _dflts['ec_sample_frequency']),
+             ("input_directory", _dflts["ec_input_directory"]),
+             ("file_pattern", _dflts["ec_file_pattern"]),
+             ("colnames", _dflts["ec_colnames"]),
+             ("sample_frequency", _dflts["ec_sample_frequency"]),
          ))
      ),
-        ('EC Outputs',
+        ("EC Outputs",
          OrderedDict((
-             ('summary_file', _dflts['ec_summary_file']),
+             ("summary_file", _dflts["ec_summary_file"]),
          ))
      ),
-        ('EC Despiking',
+        ("EC Despiking",
          OrderedDict((
-             ('despike_win_width', _dflts['ec_despike_win_width']),
-             ('despike_step', _dflts['ec_despike_step']),
-             ('despike_nreps', _dflts['ec_despike_nreps']),
+             ("despike_win_width", _dflts["ec_despike_win_width"]),
+             ("despike_step", _dflts["ec_despike_step"]),
+             ("despike_nreps", _dflts["ec_despike_nreps"]),
          ))
      ),
-        ('EC Motion Correction',
+        ("EC Motion Correction",
          OrderedDict((
-             ('motion2anemometer_pos',
-              _dflts['motion2anemometer_pos']),
-             ('complementary_filter_period',
-              _dflts['ec_complementary_filter_period']),
-             ('accel_highpass_cutoff',
-              _dflts['ec_accel_highpass_cutoff']),
+             ("motion2anemometer_pos",
+              _dflts["motion2anemometer_pos"]),
+             ("complementary_filter_period",
+              _dflts["ec_complementary_filter_period"]),
+             ("accel_highpass_cutoff",
+              _dflts["ec_accel_highpass_cutoff"]),
          ))
      ),
-        ('UW Inputs',
+        ("UW Inputs",
          OrderedDict((
-             ('input_directory', _dflts['uw_input_directory']),
-             ('file_pattern', _dflts['uw_file_pattern']),
-             ('colnames', _dflts['uw_colnames']),
+             ("input_directory", _dflts["uw_input_directory"]),
+             ("file_pattern", _dflts["uw_file_pattern"]),
+             ("colnames", _dflts["uw_colnames"]),
          ))
      ),
-        ('UW Outputs',
+        ("UW Outputs",
          OrderedDict((
-             ('summary_file', _dflts['uw_summary_file']),
+             ("summary_file", _dflts["uw_summary_file"]),
          ))
      ),))
 
@@ -219,15 +219,15 @@ def parse_config(cfg_file):
             opt_value = config.get(sec, opt)
             # Just replace and skip if we have the same as defaults
             if (dflt_dict[sec][opt] == opt_value):
-                py_dict[sec][opt] = opt_value.split(',')
+                py_dict[sec][opt] = opt_value.split(",")
             else:
                 # Otherwise move on and remove double quotes, newlines, and
                 # spaces
-                clean_opt = re.sub('["\n ]+', '', opt_value)
+                clean_opt = re.sub('["\n ]+', "", opt_value)
                 config.set(sec, opt, clean_opt)
                 # Replace values with lists, splitting on comma character,
                 # on our local dictionary
-                py_dict[sec][opt] = clean_opt.split(',')
+                py_dict[sec][opt] = clean_opt.split(",")
             # Extract single elements, and convert to floats and arrays where
             # appropriate
             if (len(py_dict[sec][opt]) == 1):
@@ -238,32 +238,32 @@ def parse_config(cfg_file):
                 py_dict[sec][opt] = [float(x) for x in py_dict[sec][opt]]
 
     # Check input directories exist
-    if (not osp.exists(py_dict['EC Inputs']['input_directory'])):
+    if (not osp.exists(py_dict["EC Inputs"]["input_directory"])):
         raise Exception("Input directory doesn't exist")
-    if (not osp.exists(py_dict['UW Inputs']['input_directory'])):
+    if (not osp.exists(py_dict["UW Inputs"]["input_directory"])):
         raise Exception("Input directory doesn't exist")
 
     # Check if we have a valid sampling frequency
-    if (py_dict['EC Inputs']['sample_frequency'] <= 0):
+    if (py_dict["EC Inputs"]["sample_frequency"] <= 0):
         raise Exception("Sampling frequency must be greater than zero")
 
     # Check if we have a valid despiking parameters
-    if (py_dict['EC Despiking']['despike_win_width'] <= 0):
+    if (py_dict["EC Despiking"]["despike_win_width"] <= 0):
         raise Exception("Despiking window width must be greater than zero")
-    if (py_dict['EC Despiking']['despike_step'] <= 0):
+    if (py_dict["EC Despiking"]["despike_step"] <= 0):
         raise Exception("Despiking step size must be greater than zero")
-    if (py_dict['EC Despiking']['despike_nreps'] < 0):
+    if (py_dict["EC Despiking"]["despike_nreps"] < 0):
         raise Exception("The number of despiking iterations cannot be negative")
 
     # Sort input file lists
-    ec_input_files = glob.glob(osp.join(config['EC Inputs']['input_directory'],
-                                        config['EC Inputs']['file_pattern']))
+    ec_input_files = glob.glob(osp.join(config["EC Inputs"]["input_directory"],
+                                        config["EC Inputs"]["file_pattern"]))
     ec_input_files.sort()
-    py_dict['EC Inputs']['input_files'] = ec_input_files
-    uw_input_files = glob.glob(osp.join(config['UW Inputs']['input_directory'],
-                                        config['UW Inputs']['file_pattern']))
+    py_dict["EC Inputs"]["input_files"] = ec_input_files
+    uw_input_files = glob.glob(osp.join(config["UW Inputs"]["input_directory"],
+                                        config["UW Inputs"]["file_pattern"]))
     uw_input_files.sort()
-    py_dict['UW Inputs']['input_files'] = uw_input_files
+    py_dict["UW Inputs"]["input_files"] = uw_input_files
 
     # Check if we have all legal names for header of input files (don't
     # care about first 2 time columns).
--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
 
-setup(name='fluxer',
-      version='0.1.0',
-      description='Tools to process flux (eddy covariance) data collected by CEOS.',
-      author='Sebastian Luque',
-      author_email='sebastian.luque@umanitoba.ca',
-      url='https://github.com/UofM-CEOS/flux_capacitor',
-      packages=['fluxer', 'fluxer.eddycov', 'fluxer.underway'],
-      # package_data={'flux': ['flux/config/flux_default.cfg']},
+# fluxer_configs = ["fluxer/config/flux_default.cfg",
+#                   "fluxer/config/underway_default.cfg"]
+setup(name="fluxer",
+      version="0.1.0",
+      description="Tools to process flux (eddy covariance) data collected by CEOS.",
+      author="Sebastian Luque",
+      author_email="sebastian.luque@umanitoba.ca",
+      url="https://github.com/UofM-CEOS/flux_capacitor",
+      packages=["fluxer", "fluxer.eddycov", "fluxer.underway"],
+      # package_data={'fluxer': fluxer_configs},
       # data_files=[('config', ['config/flux_default.cfg',
       #                         'config/underway_default.cfg'])],
       )
