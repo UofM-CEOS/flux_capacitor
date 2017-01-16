2139fc93945fe6189167edc74eb1842c732a2e8d
Sebastian Luque <spluque@gmail.com>
2017-01-16 04:14:23 -0600

    Incorporate planar fit tilt angles computation and new argument to plot

--- fluxer/eddycov/tilt_windows.py
+++ fluxer/eddycov/tilt_windows.py
@@ -6,13 +6,24 @@
 
 import itertools as itrt
 import datetime
+import os.path as osp
+import logging
 import numpy as np
 import pandas as pd
+from scipy.stats import circmean
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from .settings import FLUX_FLAGS
+from fluxer.eddycov.settings import FLUX_FLAGS
+from fluxer.eddycov.parse_ecfile import (prepare_period, NavigationError,
+                                         SonicError)
+from fluxer.eddycov.flux import planarfit
 
 __all__ = ["TiltWindows"]
+
+logger = logging.getLogger(__name__)
+# Add the null handler if importing as library; whatever using this library
+# should set up logging.basicConfig() as needed
+logger.addHandler(logging.NullHandler())
 
 
 def _fname2time(fname):
@@ -60,18 +71,29 @@ class TiltWindows:
         """
         win_files = _make_windows(ec_files, win_minutes)
         # Set up dataframe to place tilt angles and other details
-        cols = (["nfiles", "nfiles_ok"] +
+        cols = (["failed_tilt_flag", "nfiles", "nfiles_ok"] +
                 ["n{0}".format(i) for i in FLUX_FLAGS] +
                 ["phi_motion", "theta_motion", "phi_sonic", "theta_sonic"] +
                 ["wind_direction"])
         tilts = pd.DataFrame(index=[x[0] for x in win_files], columns=cols)
-        tilts.loc[:, cols[0]] = [len(x[1]) for x in win_files]
+        tilts.loc[:, cols[1]] = [len(x[1]) for x in win_files]
         tilts.loc[:, cols[-5:]] = tilts[cols[-5:]].astype(np.float).values
-        # Zero all the columns except the nfiles and parameters
-        tilts.loc[:, cols[1:-5]] = 0
+        # Zero all the columns except nfiles and parameters
+        tilts.loc[:, cols[0]] = False
+        tilts.loc[:, cols[2:-5]] = 0
         self.width = win_minutes
         self.tilts = tilts
         self.win_files = win_files
+        # [Original comment: create flags for the 6 possible sources of
+        # "bad" data, flag=0 means data good] in each input file
+        flags = dict.fromkeys(FLUX_FLAGS, False)
+        # We set up a dataframe with all files to process as index, and all
+        # the flags as columns.  This is the basis for our summary output
+        # file; other columns (e.g. flux summary calculations for the
+        # period) will be appended as we loop.
+        prep_flags = pd.DataFrame(flags,
+                                  index=[osp.basename(x) for x in ec_files])
+        self.prep_flags = prep_flags
 
     def __str__(self):
         # Show tilts attribute first, followed by the number of windows
@@ -80,39 +102,105 @@ class TiltWindows:
                "width={0.width!r})")
         return msg.format(self, len(self.win_files))
 
-    def plot(self, fig_file, title=None):
+    def get_tilts_planarfit(self, config):
+        """Compute tilt angles in all windows using planar fit method
+
+        Parameters
+        ----------
+        config : OrderedDict
+            Dictionary with parsed configuration file.
+
+        Returns
+        -------
+
+        pandas.DataFrame with tilt angles for each window.
+
+        """
+        wind_names = ["wind_speed_u", "wind_speed_v", "wind_speed_w"]
+        mot3d_names = ["acceleration_x", "acceleration_y",
+                       "acceleration_z", "rate_x", "rate_y", "rate_z"]
+
+        # Iterate over the list of tuples (key=window time stamp, file-list)
+        msg_suffix = " tilt angles via planar fit for window %s"
+        for (k, l) in self.win_files:
+            logger.info("Begin" + msg_suffix, k)
+            ec_list = []
+            ec_ok_files = []
+            # Iterate over the file-list
+            for ec_file in l:
+                logger.info("Begin preparing %s", osp.basename(ec_file))
+                # Get a file name prefix to be shared by the output files
+                # from this period.  Note iname is THE SAME AS THE INDEX IN
+                # prep_flags
+                iname = osp.basename(ec_file)
+                # Try to prepare file; set flags, and populate the list of
+                # OK files and summaries. Continue if preparation fails.
+                try:
+                    ec_prep, prep_flags = prepare_period(ec_file, config)
+                except (NavigationError, SonicError) as err:
+                    self.prep_flags.loc[iname, "failed_prep_flag"] = True
+                    for flagname, flag in err.flags.items():
+                        self.prep_flags.loc[iname, flagname] = flag
+                        self.tilts.loc[k, "n" + flagname] += np.int(flag)
+                    logger.info("Skip %s", ec_file)
+                    continue
+                else:
+                    for flagname, flag in prep_flags.iteritems():
+                        self.prep_flags.loc[iname, flagname] = flag
+                        self.tilts.loc[k, "n" + flagname] += np.int(flag)
+                ec_list.append(pd.DataFrame(ec_prep.mean()).transpose())
+                ec_ok_files.append(ec_file)
+                logger.info("End preparing %s", osp.basename(ec_file))
+
+            l = ec_ok_files
+            n_ok = len(ec_list)
+            self.tilts.loc[k, "nfiles_ok"] = n_ok
+            if n_ok < 3:
+                logger.info("Aborting window without enough adequate files %s",
+                            k)
+                for ec_file in l:
+                    self.tilts.loc[k, "failed_tilt_flag"] = True
+                continue
+            else:
+                ec_win = pd.concat(ec_list, keys=ec_ok_files)
+                logger.info("Combining %s periods in window %s", n_ok, k)
+                # Extract means from each period file for planar fitting
+                wnd = ec_win.loc[:, wind_names]
+                mot3d = ec_win.loc[:, mot3d_names[0:3]]
+                # Tilt angles for motion sensor
+                mot3d_pfit = planarfit(mot3d.values)
+                self.tilts.loc[k, "phi_motion"] = mot3d_pfit.phi
+                self.tilts.loc[k, "theta_motion"] = mot3d_pfit.theta
+                # Tilt angles for sonic anemometer
+                sonic_pfit = planarfit(wnd.values)
+                self.tilts.loc[k, "phi_sonic"] = sonic_pfit.phi
+                self.tilts.loc[k, "theta_sonic"] = sonic_pfit.theta
+                wind_direction_mean = circmean(ec_win["wind_direction"].values,
+                                               high=np.degrees(2 * np.pi))
+                self.tilts.loc[k, "wind_direction"] = wind_direction_mean
+            logger.info("End" + msg_suffix, k)
+
+    def plot(self, fig_file, step=1, title=None):
         """Generate a plot of tilt angles and diagnostics
 
         Parameters
         ----------
-        tilt_file : str
-            Path to file to read Dataframe from
         fig_file : str
             Path to file to write plot to
+        step : int
+            Step (stride) to subsample the time series by.
         title : str
             Title for plot
 
         """
-        plt.style.use('ggplot')
         # Workaround for stupid problem in Debian now:
         plt.rcParams['mathtext.fontset'] = 'stix'
 
         tilts = self.tilts
-        # # Get a scaled nfiles_ok
-        # nfiles_scaled = ((tilts["nfiles_ok"] - tilts["nfiles_ok"].min()) /
-        #                  tilts["nfiles_ok"].ptp())
-        # Example color bar
-        # cbar = axs[0].scatter(tilts.index, tilts["wind_direction"],
-        #                       c=tilts["nfiles_ok"], s=30,
-        #                       cmap=plt.cm.coolwarm)
-        # clb = fig.colorbar(cbar, ax=axs[0], orientation="horizontal",
-        #                    shrink=0.8, aspect=70, pad=0.07,
-        #                    ticks=np.arange(11), format="%d")
-        # clb.ax.set_xlabel("N periods in window", size=10)
 
         # Exclude missing data from plots so lines work
         # Set up wind direction vector
-        def plot_quiver(tseries, ax, step=1, yoffset=1, **kwargs):
+        def plot_quiver(tseries, ax, step, yoffset=1, **kwargs):
             """Return an axis with quiver plot for a time series
 
             Parameters
@@ -178,9 +266,9 @@ class TiltWindows:
         q_yticks = np.array([1, 1.1])  # quiver y ticks
         q_ylims = [np.min(q_yticks) - 0.05, np.max(q_yticks) + 0.05]
         # Top
-        plot_quiver(mot_phi, ax0, step=3, yoffset=q_yticks[0],
+        plot_quiver(mot_phi, ax0, step=step, yoffset=q_yticks[0],
                     **q_colors[0])
-        plot_quiver(wnd3d_phi, ax0, step=3, yoffset=q_yticks[1],
+        plot_quiver(wnd3d_phi, ax0, step=step, yoffset=q_yticks[1],
                     **q_colors[1])
         ax0.yaxis.set_visible(False)
         ax0.set_ylim(q_ylims)
@@ -190,9 +278,9 @@ class TiltWindows:
         ax1.set_ylabel(r"Roll $\phi$")
         ax1.xaxis.set_visible(False)
         # Bottom
-        plot_quiver(mot_theta, ax2, step=3, yoffset=q_yticks[0],
+        plot_quiver(mot_theta, ax2, step=step, yoffset=q_yticks[0],
                     **q_colors[0])
-        plot_quiver(wnd3d_theta, ax2, step=3, yoffset=q_yticks[1],
+        plot_quiver(wnd3d_theta, ax2, step=step, yoffset=q_yticks[1],
                     **q_colors[1])
         ax2.yaxis.set_visible(False)
         ax2.set_ylim(q_ylims)
